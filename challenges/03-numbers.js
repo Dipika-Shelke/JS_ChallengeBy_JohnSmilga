bcf5a0e4684d21b9404ea3b9004cba75580de7ae
Dipika-Shelke <dipikashelke.65@gmail.com>
2023-06-28 15:53:57 +0530

    array challenge

--- challenges/03-numbers.js
+++ challenges/03-numbers.js
@@ -2,6 +2,14 @@ let score1 = 45;
 let score2 = 56;
 let score3 = 87;
 let totalScore = score1 + score2 + score3;
-console.log(totalScore);
+//console.log(totalScore);
 let avgScore = totalScore/3;
-console.log(avgScore);
+console.log(totalScore, avgScore);
+
+//plates and people challenge
+let plates = 20;
+let people = 7;
+let remainingPlates = 20 % 7;
+remainingPlates++;
+let message = "There are " + remainingPlates + " plates available."
+console.log(message);
---
+++ challenges/04-array.js
@@ -0,0 +1,4 @@
+const fruits = ["Apple", "Banana", "JackFruit", "Strawberry", "custard Apple", "Mango", 23];
+const firstFruit = fruits[0];
+fruits[6] = "lemon";
+console.log(firstFruit, fruits);
