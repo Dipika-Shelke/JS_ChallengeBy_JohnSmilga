d92a73fc7bfc1f4c842a2150479f95fd55f64010
Dipika-Shelke <dipikashelke.65@gmail.com>
2023-06-29 20:58:31 +0530

    truthy and falsy

---
+++ concepts/truthy-falsy.js
@@ -0,0 +1,10 @@
+//truthy and falsy values
+// "" , '', ``, 0 , -0, NaN, false, null, undefined
+
+const text = undefined;
+
+if(text){
+    console.log('Value is truthy!');
+}else{
+    console.log('value is Falsy!');
+}
